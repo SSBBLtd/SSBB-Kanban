8665f7808711a1cec479afeba6dca2104678e787
ssbbgroupltd <ssbbgroupltd@ba50b892-06cc-47b5-9d9c-b502db45009c>
2014-09-20 16:15:29 +0000

    IS - retrieves data from database!!!! :D
    git-svn-id: https://subversion.assembla.com/svn/ssbb-kanban@10 ba50b892-06cc-47b5-9d9c-b502db45009c

--- src.main.java/main.java
+++ src.main.java/main.java
@@ -1,16 +1,16 @@
+import java.sql.SQLException;
+
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
-
 public class main {
 
-	
-	public static void main(String[] args) {
-		ApplicationContext context = 
-	    		new ClassPathXmlApplicationContext("context.xml");
-	 
-	        trial trialBean = (trial) context.getBean("trialBean");
-	        System.out.println(trialBean.findByID());
+	public static void main(String[] args) throws SQLException {
+		ApplicationContext context = new ClassPathXmlApplicationContext(
+				"context.xml");
+
+		trial trialBean = (trial) context.getBean("trialBean");
+		System.out.println(trialBean.findByID());
 	}
-	
+
 }
--- src.main.java/trial.java
+++ src.main.java/trial.java
@@ -1,4 +1,3 @@
-
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
@@ -6,45 +5,50 @@ import java.sql.SQLException;
 
 import javax.sql.DataSource;
 
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.support.ClassPathXmlApplicationContext;
-
 public class trial {
 
 	private DataSource dataSource;
- 
+
 	public void setDataSource(DataSource dataSource) {
 		this.dataSource = dataSource;
 	}
- 
 
-	public String findByID() {
-	
-		
-		String sql = "select * from roles";
+	public String findByID() throws SQLException {
+
+		String sql = "USE sql348458";
+		String sql2 = "Select * from roles";
 		Connection conn = null;
 		ResultSet rs = null;
+		ResultSet rs2 = null;
+		String result = "";
+		try {
+			conn = dataSource.getConnection();
+			PreparedStatement ps = conn.prepareStatement(sql);
+			PreparedStatement ps2 = conn.prepareStatement(sql2);
+			rs = ps.executeQuery();
+			rs2 = ps2.executeQuery();
+			while (rs2.next()) {
+
+				result += "\n" + rs2.getString("role");
+
+			}
+
+			// ps.executeUpdate();
+
+			ps.close();
+
+		} catch (SQLException e) {
+			throw new RuntimeException(e);
+
+		} finally {
+			if (conn != null) {
 				try {
-					conn = dataSource.getConnection();
-					PreparedStatement ps = conn.prepareStatement(sql);
-					String sql2 = "Select * from roles";
-					rs = ps.executeQuery();
-					ps.executeUpdate();
-					
-					
-					ps.close();
-		 
+					conn.close();
 				} catch (SQLException e) {
-					throw new RuntimeException(e);
-		 
-				} finally {
-					if (conn != null) {
-						try {
-							conn.close();
-						} catch (SQLException e) {}
-					}
 				}
-		
-		return rs.toString();
+			}
+		}
+
+		return result;
 	}
 }
