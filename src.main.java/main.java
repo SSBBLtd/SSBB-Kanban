ed51fdbe739d4d9cd1e45bd3f137bac8cbe0bb88
ssbbgroupltd <ssbbgroupltd@ba50b892-06cc-47b5-9d9c-b502db45009c>
2014-09-20 15:19:55 +0000

    RT - added trial classes and methods to try to connect to database
    git-svn-id: https://subversion.assembla.com/svn/ssbb-kanban@6 ba50b892-06cc-47b5-9d9c-b502db45009c

---
+++ src.main.java/main.java
@@ -0,0 +1,16 @@
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+
+public class main {
+
+	
+	public static void main(String[] args) {
+		ApplicationContext context = 
+	    		new ClassPathXmlApplicationContext("context.xml");
+	 
+	        trial trialBean = (trial) context.getBean("trialBean");
+	        System.out.println(trialBean.findByID());
+	}
+	
+}
---
+++ src.main.java/trial.java
@@ -0,0 +1,47 @@
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import javax.sql.DataSource;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class trial {
+
+	private DataSource dataSource;
+ 
+	public void setDataSource(DataSource dataSource) {
+		this.dataSource = dataSource;
+	}
+ 
+
+	public String findByID() {
+	
+		
+		String sql = "SELECT * FROM ROLES";
+		Connection conn = null;
+		ResultSet rs = null;
+				try {
+					conn = dataSource.getConnection();
+					PreparedStatement ps = conn.prepareStatement(sql);
+					rs = ps.executeQuery();
+					ps.executeUpdate();
+					ps.close();
+		 
+				} catch (SQLException e) {
+					throw new RuntimeException(e);
+		 
+				} finally {
+					if (conn != null) {
+						try {
+							conn.close();
+						} catch (SQLException e) {}
+					}
+				}
+		
+		return rs.toString();
+	}
+}
