7c38147c57ae57aca87f7a15a849399c0c245bed
ssbbgroupltd <ssbbgroupltd@ba50b892-06cc-47b5-9d9c-b502db45009c>
2014-10-05 18:01:54 +0000

    RT - added working entity manager feature
    git-svn-id: https://subversion.assembla.com/svn/ssbb-kanban@15 ba50b892-06cc-47b5-9d9c-b502db45009c

--- src/main/java/projectTrial.java
+++ src/main/java/projectTrial.java
@@ -1,7 +1,9 @@
 import java.util.List;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.EntityManager;
+import javax.persistence.Id;
 import javax.persistence.NamedQuery;
 import javax.persistence.PersistenceContext;
 import javax.persistence.Query;
@@ -12,27 +14,23 @@ import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
 
 @Entity
 @Table(name="roles")
-@NamedQuery(name="roles.findAll", query="SELECT r FROM roles r")
+//@NamedQuery(name="roles.findAll", query="SELECT r FROM roles r")
 public class projectTrial {
 
-	EntityManager em;
+
 	
-	@PersistenceContext
-	public void setEntityManager(EntityManager em){
-		this.em = em;
-	}
 	
-	org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter adap = new HibernateJpaVendorAdapter();
-	
-	public List<String> getRoles() {
+	/*public List<String> getRoles() {
 		Query query = em.createNativeQuery("Select * from roles");
 		List<String> results = (List<String> )query.getResultList();
 		return results;
-	}
+	}*/
 	
 	public String trymeho() {
 		
 		return null;
 	}
-	
+	@Id
+	@Column(name = "role_id")
+	public int id;
 }
