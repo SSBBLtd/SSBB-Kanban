9ae9bd9dda85170d7d9411ad4041191a60a2c648
ssbbgroupltd <ssbbgroupltd@ba50b892-06cc-47b5-9d9c-b502db45009c>
2014-10-19 16:37:35 +0000

    IS - changed index.jsp to home.jsp and autowired role to homecontroller
    git-svn-id: https://subversion.assembla.com/svn/ssbb-kanban@21 ba50b892-06cc-47b5-9d9c-b502db45009c

--- src/main/java/com/ssbb/kanban/controller/HomeController.java
+++ src/main/java/com/ssbb/kanban/controller/HomeController.java
@@ -1,18 +1,32 @@
 package com.ssbb.kanban.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.ssbb.kanban.data.impl.Role;
 
 @Controller
 public class HomeController {
 
+	@Autowired
+	private Role role;
+
 	@RequestMapping(value = "/home")
-	public String loadHome() {
-		return "home";
+	public String loadHome(ModelMap map) {
+		map.put("role", role);
+		return "/home";
 	}
 
-	@RequestMapping(value = "login")
+	/*
+	 * @RequestMapping(value = "/home") public String load() { return "home"; }
+	 */
+
+	@RequestMapping(value = "login", method = RequestMethod.POST)
 	public String login() {
+
 		return "landing";
 	}
 
