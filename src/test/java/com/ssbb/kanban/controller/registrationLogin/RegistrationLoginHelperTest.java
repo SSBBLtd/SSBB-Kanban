bdd21f4a9a0286687fa09d3a52baa0a74dccfe70
ssbbgroupltd <ssbbgroupltd@ba50b892-06cc-47b5-9d9c-b502db45009c>
2015-01-25 21:28:32 +0000

    RT - 100% Test coverage of RegistrationController and RegistrationLoginHelper. Oh yeah!
    git-svn-id: https://subversion.assembla.com/svn/ssbb-kanban@36 ba50b892-06cc-47b5-9d9c-b502db45009c

--- src/test/java/com/ssbb/kanban/controller/registrationLogin/RegistrationLoginHelperTest.java
+++ src/test/java/com/ssbb/kanban/controller/registrationLogin/RegistrationLoginHelperTest.java
@@ -58,4 +58,9 @@ public class RegistrationLoginHelperTest {
 		assertTrue(helper.passwordCorrect(password));
 	}
 
+	@Test
+	public void testHashPw() {
+		helper.hashpw(user);
+		verify(user).setPassword(anyString());
+	}
 }
