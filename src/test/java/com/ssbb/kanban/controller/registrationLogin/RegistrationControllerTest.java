ed72b9bb493fc290995d58cba1b67b0a4cd3d6cd
ssbbgroupltd <ssbbgroupltd@ba50b892-06cc-47b5-9d9c-b502db45009c>
2015-02-01 14:00:52 +0000

    RT - committed RegControllerTest
    git-svn-id: https://subversion.assembla.com/svn/ssbb-kanban@37 ba50b892-06cc-47b5-9d9c-b502db45009c

---
+++ src/test/java/com/ssbb/kanban/controller/registrationLogin/RegistrationControllerTest.java
@@ -0,0 +1,69 @@
+package com.ssbb.kanban.controller.registrationLogin;
+
+import static org.junit.Assert.*;
+import static org.mockito.Matchers.anyString;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import javax.servlet.http.HttpSession;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
+import org.springframework.ui.ModelMap;
+
+import com.ssbb.kanban.dao.impl.UserDAO;
+import com.ssbb.kanban.data.impl.User;
+
+@RunWith(MockitoJUnitRunner.class)
+public class RegistrationControllerTest {
+
+	@Mock
+	UserDAO userDAO;
+	
+	@Mock
+	User user;
+	
+	@Mock
+	RegistrationLoginHelper helper;
+	
+	@InjectMocks
+	RegistrationController controller;
+	
+	@Test
+	public void testLoadRegisterMethod() {
+		
+		ModelMap mockedMap = mock(ModelMap.class);
+		
+		String result = controller.loadRegister(mockedMap);
+		verify(mockedMap).addAttribute(user);
+		assertEquals("register", result);
+	}
+
+	@Test
+	public void testRegisterWhenUserNotExist() {
+		
+		HttpSession mockSession = mock(HttpSession.class);
+		User mockedUser = mock(User.class);
+		when(mockedUser.getEmail()).thenReturn("email");
+		
+		String result = controller.register(mockSession, mockedUser);
+		
+		verify(mockedUser).setLoggedIn(true);
+		verify(helper).hashpw(mockedUser);
+		verify(userDAO).add(mockedUser);
+		verify(mockSession).setAttribute("user", mockedUser);;
+		assertEquals("redirect:/landing", result);
+	}
+	
+	@Test
+	public void testRegisterWhenUserExists() {
+		when(helper.userExists(anyString())).thenReturn(true);
+		
+		String result = controller.register(mock(HttpSession.class), user);
+		assertEquals("redirect:/home", result);
+	}
+}
