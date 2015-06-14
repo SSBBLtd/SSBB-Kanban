34153887d6b70c8e58c4af53b12daabae0c7df03
ssbbgroupltd <ssbbgroupltd@ba50b892-06cc-47b5-9d9c-b502db45009c>
2015-06-14 14:02:37 +0000

    RT - refactored and reduced boilerplate code
    git-svn-id: https://subversion.assembla.com/svn/ssbb-kanban@55 ba50b892-06cc-47b5-9d9c-b502db45009c

--- src/main/java/com/ssbb/kanban/utils/SQLHelper.java
+++ src/main/java/com/ssbb/kanban/utils/SQLHelper.java
@@ -20,29 +20,11 @@ public class SQLHelper {
 	 */
 	public static String formInsertStatement(String table, String[] columns, String[] values) {
 		StringBuffer statement = new StringBuffer();
-		statement.append(INSERT);
-		statement.append(table + "(");
+		statement.append(INSERT).append(table).append("(");
 		
 		if( columns.length == values.length ) {
-			for(int i = 0; i < columns.length; i++) {
-				statement.append(columns[i]);
-				if( i == columns.length - 1) {
-					statement.append(") ");
-					break;
-				}
-				statement.append(", ");
-			}
-			
-			statement.append("VALUES(");
-			for(int i = 0; i < values.length; i++) {
-				statement.append(values[i]);
-				if( i == values.length - 1) {
-					statement.append(")");
-					break;
-				}
-				statement.append(", ");
-			}
-			
+			appendArray(statement, columns).append("VALUES(");
+			appendArray(statement, values);
 			return statement.toString();
 		}
 		throw new IllegalArgumentException("Number of columns is different from number of values.\n" 
@@ -50,8 +32,21 @@ public class SQLHelper {
 	}
 	
 	public static void checkForSecurityKeywords(String value) {
+			
+	}
 	
-		
+	private static StringBuffer appendArray(StringBuffer statement, String[] array) {
+		int i = 0;
+		while(true) {			
+			statement.append(array[i]);
+			if( i == array.length - 1) {
+				statement.append(") ");
+				break;
+			}
+			statement.append(", ");
+			i++;
+		}
+		return statement;
 	}
 	
 }
