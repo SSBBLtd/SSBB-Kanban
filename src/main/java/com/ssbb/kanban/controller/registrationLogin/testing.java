cef316980d773b097f104d634b12be1c358fcf28
ilya123s <ilyaskliarov@hotmail.com>
2015-09-26 18:58:48 +0100

    closing review comments

--- src/main/java/com/ssbb/kanban/controller/registrationLogin/testing.java
+++ src/main/java/com/ssbb/kanban/controller/registrationLogin/testing.java
@@ -2,4 +2,9 @@ package com.ssbb.kanban.controller.registrationLogin;
 
 public class testing {
 
+	public testing() {
+
+		System.out
+				.println("no, this class should not contain any logic, please refer to the design for further details.");
+	}
 }
