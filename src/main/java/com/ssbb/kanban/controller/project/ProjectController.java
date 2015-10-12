fd6e9e6fd94078d4b5f3bad3459581ff1de7ecad
ilya123s <ilyaskliarov@hotmail.com>
2015-10-12 10:29:57 +0100

    Added UserHelper to utils folder for general user utility methods. Added javadocs and moved logic from project controller to new UserHelper

--- src/main/java/com/ssbb/kanban/controller/project/ProjectController.java
+++ src/main/java/com/ssbb/kanban/controller/project/ProjectController.java
@@ -12,54 +12,78 @@ import com.ssbb.kanban.Constants;
 import com.ssbb.kanban.dao.impl.ProjectDAO;
 import com.ssbb.kanban.data.impl.Project;
 import com.ssbb.kanban.data.impl.User;
+import com.ssbb.kanban.utils.UserHelper;
 
 @Controller
 public class ProjectController {
 
-	@Autowired
-	private Project project;
+    @Autowired
+    private Project project;
 
-	@Autowired
-	private ProjectDAO projectDAO;
+    @Autowired
+    private ProjectDAO projectDAO;
 
-	@RequestMapping(value = "project", method = RequestMethod.GET)
-	public String loadProject(ModelMap map, HttpSession session) {
+    private final String PAGE_ID = "project";
 
-		return "project";
-	}
+    /**
+     * This method is called when a user selects a project from their list of
+     * projects. This will find the project selected and add it to model map to
+     * be used by the front end to be used by the front end to display that
+     * project page.
+     * 
+     * @param map
+     * @param session
+     * @param submittedProject
+     * @return String Page ID to load
+     */
+    @RequestMapping(value = "loadSelectedProject", method = RequestMethod.POST)
+    public String loadSelectedProject(ModelMap map, HttpSession session, Project submittedProject) {
 
-	@RequestMapping(value = "loadSelectedProject", method = RequestMethod.POST)
-	public String loadSelectedProject(ModelMap map, HttpSession session,
-			Project submittedProject) {
+        //get user from the session.
+        User user = (User) session.getAttribute(Constants.USER);
 
-		User user = (User) session.getAttribute(Constants.USER);
-		for (Project userProject : user.getProjects()) {
-			if (userProject.getName().equals(submittedProject.getName())) {
-				map.addAttribute(Constants.PROJECT, userProject);
-			}
-		}
+        //get the project from the user's projects.
+        Project projectSelected = UserHelper.getUserSelectProjectByName(user, submittedProject.getName());
 
-		return loadProject(map, session);
+        if (projectSelected == null) {
+            //TO DO, must return some error/error page.
+        }
 
-	}
+        //add project to map to be displayed.
+        map.addAttribute(Constants.PROJECT, projectSelected);
 
-	@RequestMapping(value = "createProject", method = RequestMethod.POST)
-	public String createProject(ModelMap map, HttpSession session,
-			Project project) {
+        return PAGE_ID;
 
-		// Add the user who create the project to the project's user list.
-		User user = (User) session.getAttribute(Constants.USER);
-		project.getUserList().add(user);
+    }
 
-		// persist the project
-		projectDAO.add(project);
+    /**
+     * Method is called when a user selects to create a new project. This will
+     * get the project created from the front end and persist it into the
+     * database as well as add it to the user's list of projects. The project
+     * created will then be added to the model map to be used by the front end
+     * to display that project page.
+     * 
+     * @param map
+     * @param session
+     * @param project
+     * @return String Page ID to load
+     */
+    @RequestMapping(value = "createProject", method = RequestMethod.POST)
+    public String createProject(ModelMap map, HttpSession session, Project project) {
 
-		// add the project to the list of the user's projects.
-		user.getProjects().add(project);
+        // Add the user who create the project to the project's user list.
+        User user = (User) session.getAttribute(Constants.USER);
+        project.getUserList().add(user);
 
-		map.addAttribute(Constants.PROJECT, project);
+        // persist the project
+        projectDAO.add(project);
 
-		return loadProject(map, session);
-	}
+        // add the project to the list of the user's projects.
+        user.getProjects().add(project);
+
+        map.addAttribute(Constants.PROJECT, project);
+
+        return PAGE_ID;
+    }
 
 }
